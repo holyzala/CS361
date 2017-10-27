c7786c4d77f2b2707582eb619dd9f265dd26edba
Christopher Magnuson <magnus32@uwm.edu>
2017-10-27 14:58:16 -0500

    #9 Add some tests for admin login

--- project/CLI.py
+++ project/CLI.py
@@ -9,6 +9,25 @@ class CLI():
         return ''
 
 
-class TestCLI(unittest.TestCase):
+class TestLogin(unittest.TestCase):
     def setUp(self):
         self.cli = CLI()
+
+    def test_login_success(self):
+        self.assertEqual("Login successful", self.cli.command("login gamemaker 1234"), "Login return message not correct")
+
+    def test_login_bad_password(self):
+        self.assertEqual("Login failed", self.cli.command("login gamemaker 4321"), "Login return message not correct")
+
+    def test_login_bad_username(self):
+        self.assertEqual("Login failed", self.cli.command("login ___ 1234"), "Login return message not correct")
+
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestLogin))
+    runner = unittest.TextTestRunner()
+    res = runner.run(suite)
+    print(res)
+    print("*" * 20)
+    for i in res.failures: print(i[1])
--- project/GameMaster.py
+++ project/GameMaster.py
@@ -4,16 +4,41 @@ import unittest
 class GameMaker():
     def __init__(self):
         self.game = None
-        self.username = ""
-        self.password = ""
+        self.username = "gamemaker"
+        self.password = "1234"
 
-    def login(self):
+    def login(self, username, password):
         return "", False
 
     def create_game(self):
         return None
 
 
-class TestGameMaker(unittest.TestCase):
+class TestLogin(unittest.TestCase):
     def setUp(self):
         self.gm = GameMaker()
+
+    def test_login_success(self):
+        username, isadmin = self.gm.login("gamemaker", "1234")
+        self.assertTrue(isadmin, "Admin flag not set")
+        self.assertEqual("gamemaker", username, "Incorrect username returned")
+
+    def test_login_bad_password(self):
+        username, isadmin = self.gm.login("gamemaker", "4321")
+        self.assertFalse(isadmin, "Admin flag incorrectly set")
+        self.assertEqual("", username, "Username has value when it should be empty")
+
+    def test_login_bad_username(self):
+        username, isadmin = self.gm.login("___", "1234")
+        self.assertFalse(isadmin, "Admin flag incorrectly set")
+        self.assertEqual("", username, "Username has value when it should be empty")
+
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestLogin))
+    runner = unittest.TextTestRunner()
+    res = runner.run(suite)
+    print(res)
+    print("*" * 20)
+    for i in res.failures: print(i[1])
