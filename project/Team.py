aa01def7178ee45af40ed3a55deadc6bda76e8ec
Martin Soto <martin.soto414@gmail.com>
2017-10-29 12:20:55 -0500

    Added test

--- project/Team.py
+++ project/Team.py
@@ -6,7 +6,7 @@ class Team():
         self.username = ""
         self.password = ""
 
-    def login(self, password):
+    def login(self, username, password):
         return "", False
 
     def answer_question(self, answer):
@@ -22,3 +22,27 @@ class Team():
 class TestTeam(unittest.TestCase):
     def setUp(self):
         self.team = Team()
+    
+    def test_team_login_success(self):
+        self.username = "team1"
+        self.password = "password123"
+      
+        self.assertTrue(login(self, "team1", "password123"))
+                         
+    def test_team_login_fail(self):
+       self.username = "team1"
+       self.password = "password123"
+        
+       self.assertFalse(login(self, "team1", "random"))
+    
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestLogin))
+    runner = unittest.TextTestRunner()
+    res = runner.run(suite)
+    print(res)
+    print("*" * 20)
+    for i in res.failures: print(i[1]) 
+    
+                          
+                    
