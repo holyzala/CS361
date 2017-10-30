620511f3974f1a20e097e50fd4b4455ca20297a5
Martin Soto <martin.soto414@gmail.com>
2017-10-30 17:36:54 -0500

    Update CLI.py

--- project/CLI.py
+++ project/CLI.py
@@ -1,14 +1,61 @@
+
 import unittest
+from GameMaker import GMFactory
 
 
-class CLI():
+class CLI:
     def __init__(self):
-        self.gm = None
+        self.gm = GMFactory().getGM()
+        self.current_user = ""
+        self.is_gm = False
+        self.game = None
+
+    def __login(self, args):
+        try:
+            self.current_user, self.is_gm = self.gm.login(args[1], args[2])
+        except IndexError:
+            return "Invalid parameters"
+        if self.current_user is "":
+            return "Login failed"
+        return "Login successful"
 
     def command(self, args):
-        return ''
+        commands = {"login": self.__login}
+        inp = args.split(" ")
+        try:
+            return commands[inp[0].lower()](inp)
+        except KeyError:
+            return "Invalid command"
 
+      
 
-class TestCLI(unittest.TestCase):
+class TestLogin(unittest.TestCase):
     def setUp(self):
         self.cli = CLI()
+
+    def test_login_success(self):
+        self.assertEqual("Login successful", self.cli.command("login gamemaker 1234"), "Login return message not correct")
+
+    def test_login_bad_password(self):
+        self.assertEqual("Login failed", self.cli.command("login gamemaker 4321"), "Login return message not correct")
+
+    def test_login_bad_username(self):
+        self.assertEqual("Login failed", self.cli.command("login ___ 1234"), "Login return message not correct")
+
+    def test_login_bad_args(self):
+        self.assertEqual("Invalid parameters", self.cli.command("login gamemaker"), "Invalid parameters")
+        
+    def test_team_add(self):
+        self.is_gm = True;
+        
+        
+
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestLogin))
+    runner = unittest.TextTestRunner()
+    res = runner.run(suite)
+    print(res)
+    print("*" * 20)
+    for i in res.failures: print(i[1])
