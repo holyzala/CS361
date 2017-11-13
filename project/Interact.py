6dbc227114156d602253dda33b89805ecbfcc509
Christopher Magnuson <magnus32@uwm.edu>
2017-11-13 14:32:24 -0600

    Some rudimentary printing in Interact.py

--- project/Interact.py
+++ project/Interact.py
@@ -1,7 +1,37 @@
 import CLI
+import shlex
 
 
 if __name__ == "__main__":
     cli = CLI.CLI(CLI.COMMANDS)
     while True:
-        print(cli.command(input("> ")))
+        inp = input("> ")
+        tokenized = shlex.split(inp.lower())
+        try:
+            if tokenized[0] == 'print':
+                if tokenized[1] == 'cli':
+                    print(vars(cli))
+                elif tokenized[1] == 'game':
+                    if cli.game:
+                        print(vars(cli.game))
+                    else:
+                        print(None)
+                elif tokenized[1] == 'gamemaker':
+                    print(vars(cli.game_maker))
+                elif tokenized[1] == 'team':
+                    if cli.game:
+                        print(vars(cli.game.teams[tokenized[2]]))
+                    else:
+                        print(None)
+                elif tokenized[1] == 'landmark':
+                    if cli.game:
+                        for landmark in cli.game.landmarks:
+                            if landmark.clue == tokenized[2]:
+                                print(vars(landmark))
+                    else:
+                        print(None)
+                continue
+        except IndexError:
+            continue
+
+        print(cli.command(inp))
